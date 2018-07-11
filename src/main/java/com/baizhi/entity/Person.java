f47a63930d59f5707219ef531237d7191a6c6e0e
tjh <1573065243@qq.com>
2018-07-11 21:56:49 +0800

    第二次提交

--- src/main/java/com/baizhi/entity/Person.java
+++ src/main/java/com/baizhi/entity/Person.java
@@ -4,4 +4,14 @@ package com.baizhi.entity;
  * Created by Administrator on 2018/7/11.
  */
 public class Person {
+    private  String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
