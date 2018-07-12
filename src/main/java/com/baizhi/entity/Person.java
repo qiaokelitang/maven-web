4bf53db37f81d685d9865d667a19e4032c6ee5fe
tjh <1573065243@qq.com>
2018-07-12 19:51:06 +0800

    第三次提交

--- src/main/java/com/baizhi/entity/Person.java
+++ src/main/java/com/baizhi/entity/Person.java
@@ -5,7 +5,15 @@ package com.baizhi.entity;
  */
 public class Person {
     private  String name;
+    private  String password;
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getName() {
         return name;
